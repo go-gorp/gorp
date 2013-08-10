3aa59f87d2d4ba141c81f0d892fc041a25ef649d
Jochen Voss <voss@seehuhn.de>
2013-08-10 23:30:03 +0100

    Add support for time.Time objects to gorp.
    This commit adds support for storing and retriving time.Time objects,
    by passing through such objects to the underlying driver.  This
    resolves gorp issue #14.
    
    The commit is tested and works with the github.com/mattn/go-sqlite3
    driver.
    
    The commit does not currently work with the
    github.com/ziutek/mymysql/godrv driver, due to a bug in the mysql
    driver.  This problem is resolved by mymysql pull request #77.  I have
    successfully tested that with the fix from pull request #77 applied to
    mymysql, and with the current commit applied, gorp can correctly store
    and retrieve time.Time objects from mysql databases.
    
    This commit is NOT tested with the github.com/lib/pq driver.

--- dialect.go
+++ dialect.go
@@ -85,6 +85,8 @@ func (d SqliteDialect) ToSqlType(val reflect.Type, maxsize int, isAutoIncr bool)
 		return "integer"
 	case "NullableBytes":
 		return "blob"
+	case "Time":
+		return "datetime"
 	}
 
 	if maxsize < 1 {
@@ -170,6 +172,8 @@ func (d PostgresDialect) ToSqlType(val reflect.Type, maxsize int, isAutoIncr boo
 		return "smallint"
 	case "NullableBytes":
 		return "bytea"
+	case "Time", "NullTime":
+		return "timestamp"
 	}
 
 	if maxsize < 1 {
@@ -264,6 +268,8 @@ func (m MySQLDialect) ToSqlType(val reflect.Type, maxsize int, isAutoIncr bool) 
 		return "tinyint"
 	case "NullableBytes":
 		return "mediumblob"
+	case "Time":
+		return "datetime"
 	}
 
 	if maxsize < 1 {
