e77311ca56c61521b63d3c4a5e560d7f0aa32bfe
Samuel Nelson <valczir.darkvein@gmail.com>
2013-11-08 11:40:16 -0700

    Added Failing Test for Overridden Fields
    Simply mapping any table with any overridden field causes the tests to fail, at least with the postgres dialect.  This commit adds a struct with an overridden field and maps it to cause the tests to fail.

--- gorp_test.go
+++ gorp_test.go
@@ -27,6 +27,11 @@ type Invoice struct {
 	IsPaid   bool
 }
 
+type OverriddenInvoice struct {
+	Invoice
+	Id string
+}
+
 type Person struct {
 	Id      int64
 	Created int64
@@ -1538,6 +1543,7 @@ func initDbMap() *DbMap {
 	dbmap := newDbMap()
 	dbmap.TraceOn("", log.New(os.Stdout, "gorptest: ", log.Lmicroseconds))
 	dbmap.AddTableWithName(Invoice{}, "invoice_test").SetKeys(true, "Id")
+	dbmap.AddTableWithName(OverriddenInvoice{}, "invoice_override_test").SetKeys(true, "Id")
 	dbmap.AddTableWithName(Person{}, "person_test").SetKeys(true, "Id")
 	dbmap.AddTableWithName(WithIgnoredColumn{}, "ignored_column_test").SetKeys(true, "Id")
 	dbmap.AddTableWithName(TypeConversionExample{}, "type_conv_test").SetKeys(true, "Id")
