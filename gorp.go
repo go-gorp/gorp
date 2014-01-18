65a90e3fd36323cf506ccc3b87a2e7b8f644ad97
Rao Venu <venuzr1@gmail.com>
2014-01-17 22:14:17 -0500

    Fixed minor type

--- gorp.go
+++ gorp.go
@@ -929,7 +929,7 @@ An example of how to use :
 
 	type Person struct {
 		FirstName     string `db:"first_name"`
-		FirstName     string `db:"last_name"`
+		LastName     string `db:"last_name"`
 		Email     	  string `db:"email"`
 		Phone     	  string `db:"phone"`
 	}
