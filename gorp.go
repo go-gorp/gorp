6594ec31d4385c2ee320f336eda4f4573e308aef
Christian <christian.probst.dk@gmail.com>
2015-02-16 21:20:45 +0100

    Corrected vim gone wild

--- gorp.go
+++ gorp.go
@@ -1,4 +1,4 @@
-"yright 2012 James Cooper. All rights reserved.
+// Copyright 2012 James Cooper. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
