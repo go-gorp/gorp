7d6864d362a604b61ebdaeed8d07a65dd2a0a3f1
Sol Cates <scates@vormetric.com>
2017-03-17 13:45:10 -0700

    Removed debug logging

--- dialect_mysql.go
+++ dialect_mysql.go
@@ -59,7 +59,6 @@ func (d MySQLDialect) ToSqlType(val reflect.Type, maxsize int, isAutoIncr bool) 
 			return "mediumblob"
 		}
 	}
-	log.Println(val.Name())
 	switch val.Name() {
 	case "NullInt64":
 		return "bigint"
