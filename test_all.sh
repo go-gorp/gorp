a919501410229360b15907fd0acfa0b271eb9646
Samuel Nelson <valczir.darkvein@gmail.com>
2017-10-21 13:23:34 -0600

    Add crate tests

--- test_all.sh
+++ test_all.sh
@@ -5,29 +5,34 @@
 
 coveralls_testflags="-v -covermode=count -coverprofile=coverage.out"
 
-echo "Running unit tests"
-ginkgo -r -race -randomizeAllSpecs -keepGoing -- -test.run TestGorp
+#echo "Running unit tests"
+#go test -v -race -test.run TestGorp
 
-echo "Testing against mysql"
-export GORP_TEST_DSN=gorptest/gorptest/gorptest
-export GORP_TEST_DIALECT=mysql
-go test $coveralls_testflags $GOBUILDFLAG $@ .
+#echo "Testing against mysql"
+#export GORP_TEST_DSN=gorptest/gorptest/gorptest
+#export GORP_TEST_DIALECT=mysql
+#go test $coveralls_testflags $GOBUILDFLAG $@ .
 
-echo "Testing against gomysql"
-export GORP_TEST_DSN=gorptest:gorptest@/gorptest
-export GORP_TEST_DIALECT=gomysql
-go test $coveralls_testflags $GOBUILDFLAG $@ .
+#echo "Testing against gomysql"
+#export GORP_TEST_DSN=gorptest:gorptest@/gorptest
+#export GORP_TEST_DIALECT=gomysql
+#go test $coveralls_testflags $GOBUILDFLAG $@ .
 
-echo "Testing against postgres"
-export GORP_TEST_DSN="user=gorptest password=gorptest dbname=gorptest sslmode=disable"
-export GORP_TEST_DIALECT=postgres
-go test $coveralls_testflags $GOBUILDFLAG $@ .
+#echo "Testing against postgres"
+#export GORP_TEST_DSN="user=gorptest password=gorptest dbname=gorptest sslmode=disable"
+#export GORP_TEST_DIALECT=postgres
+#go test $coveralls_testflags $GOBUILDFLAG $@ .
 
-echo "Testing against sqlite"
-export GORP_TEST_DSN=/tmp/gorptest.bin
-export GORP_TEST_DIALECT=sqlite
-go test $coveralls_testflags $GOBUILDFLAG $@ .
-rm -f /tmp/gorptest.bin
+#echo "Testing against sqlite"
+#export GORP_TEST_DSN=/tmp/gorptest.bin
+#export GORP_TEST_DIALECT=sqlite
+#go test $coveralls_testflags $GOBUILDFLAG $@ .
+#rm -f /tmp/gorptest.bin
+
+echo "Testing against crate"
+export GORP_TEST_DSN=http://localhost:4200/
+export GORP_TEST_DIALECT=crate
+go test $coveralls_testflags $GOBUILDFLAG $@
 
 case $(go version) in
   *go1.4*)
