4df78490a9aa9a78b9b02b0c913df8dc1954faee
Samuel Nelson <valczir.darkvein@gmail.com>
2018-02-26 09:58:12 -0600

    Replace ginkgo with onpar (#367)
    We've been running in to more and more problems with ginkgo, and frankly, I prefer the simplicity of onpar.  Time to switch.

--- dialect_mysql_test.go
+++ dialect_mysql_test.go
@@ -13,199 +13,178 @@ package gorp_test
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"reflect"
+	"testing"
 	"time"
 
-	// ginkgo/gomega functions read better as dot-imports.
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/ginkgo/extensions/table"
-	. "github.com/onsi/gomega"
-
+	"github.com/apoydence/onpar"
+	"github.com/apoydence/onpar/expect"
+	"github.com/apoydence/onpar/matchers"
 	"github.com/go-gorp/gorp"
 )
 
-var _ = Describe("MySQLDialect", func() {
-	var (
-		engine, encoding string
-		dialect          gorp.MySQLDialect
-	)
+func TestMySQLDialect(t *testing.T) {
+	o := onpar.New()
+	defer o.Run(t)
 
-	JustBeforeEach(func() {
-		dialect = gorp.MySQLDialect{
-			Engine:   engine,
-			Encoding: encoding,
+	o.BeforeEach(func(t *testing.T) (expect.Expectation, gorp.MySQLDialect) {
+		return expect.New(t), gorp.MySQLDialect{
+			Engine:   "foo",
+			Encoding: "bar",
 		}
 	})
 
-	DescribeTable("ToSqlType",
-		func(value interface{}, maxsize int, autoIncr bool, expected string) {
-			typ := reflect.TypeOf(value)
-			sqlType := dialect.ToSqlType(typ, maxsize, autoIncr)
-			Expect(sqlType).To(Equal(expected))
-		},
-		Entry("bool", true, 0, false, "boolean"),
-		Entry("int8", int8(1), 0, false, "tinyint"),
-		Entry("uint8", uint8(1), 0, false, "tinyint unsigned"),
-		Entry("int16", int16(1), 0, false, "smallint"),
-		Entry("uint16", uint16(1), 0, false, "smallint unsigned"),
-		Entry("int32", int32(1), 0, false, "int"),
-		Entry("int (treated as int32)", int(1), 0, false, "int"),
-		Entry("uint32", uint32(1), 0, false, "int unsigned"),
-		Entry("uint (treated as uint32)", uint(1), 0, false, "int unsigned"),
-		Entry("int64", int64(1), 0, false, "bigint"),
-		Entry("uint64", uint64(1), 0, false, "bigint unsigned"),
-		Entry("float32", float32(1), 0, false, "double"),
-		Entry("float64", float64(1), 0, false, "double"),
-		Entry("[]uint8", []uint8{1}, 0, false, "mediumblob"),
-		Entry("NullInt64", sql.NullInt64{}, 0, false, "bigint"),
-		Entry("NullFloat64", sql.NullFloat64{}, 0, false, "double"),
-		Entry("NullBool", sql.NullBool{}, 0, false, "tinyint"),
-		Entry("Time", time.Time{}, 0, false, "datetime"),
-		Entry("default-size string", "", 0, false, "varchar(255)"),
-		Entry("sized string", "", 50, false, "varchar(50)"),
-		Entry("large string", "", 1024, false, "text"),
-	)
-
-	Describe("AutoIncrStr", func() {
-		It("returns the auto increment string", func() {
-			Expect(dialect.AutoIncrStr()).To(Equal("auto_increment"))
-		})
+	o.Group("ToSqlType", func() {
+		tests := []struct {
+			name     string
+			value    interface{}
+			maxSize  int
+			autoIncr bool
+			expected string
+		}{
+			{"bool", true, 0, false, "boolean"},
+			{"int8", int8(1), 0, false, "tinyint"},
+			{"uint8", uint8(1), 0, false, "tinyint unsigned"},
+			{"int16", int16(1), 0, false, "smallint"},
+			{"uint16", uint16(1), 0, false, "smallint unsigned"},
+			{"int32", int32(1), 0, false, "int"},
+			{"int (treated as int32)", int(1), 0, false, "int"},
+			{"uint32", uint32(1), 0, false, "int unsigned"},
+			{"uint (treated as uint32)", uint(1), 0, false, "int unsigned"},
+			{"int64", int64(1), 0, false, "bigint"},
+			{"uint64", uint64(1), 0, false, "bigint unsigned"},
+			{"float32", float32(1), 0, false, "double"},
+			{"float64", float64(1), 0, false, "double"},
+			{"[]uint8", []uint8{1}, 0, false, "mediumblob"},
+			{"NullInt64", sql.NullInt64{}, 0, false, "bigint"},
+			{"NullFloat64", sql.NullFloat64{}, 0, false, "double"},
+			{"NullBool", sql.NullBool{}, 0, false, "tinyint"},
+			{"Time", time.Time{}, 0, false, "datetime"},
+			{"default-size string", "", 0, false, "varchar(255)"},
+			{"sized string", "", 50, false, "varchar(50)"},
+			{"large string", "", 1024, false, "text"},
+		}
+		for _, t := range tests {
+			o.Spec(t.name, func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+				typ := reflect.TypeOf(t.value)
+				sqlType := dialect.ToSqlType(typ, t.maxSize, t.autoIncr)
+				expect(sqlType).To(matchers.Equal(t.expected))
+			})
+		}
 	})
 
-	Describe("AutoIncrBindValue", func() {
-		It("returns the value used to bind the auto-increment value", func() {
-			Expect(dialect.AutoIncrBindValue()).To(Equal("null"))
-		})
+	o.Spec("AutoIncrStr", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.AutoIncrStr()).To(matchers.Equal("auto_increment"))
 	})
 
-	Describe("AutoIncrInsertSuffix", func() {
-		It("returns the suffix needed for auto-incrementing", func() {
-			Expect(dialect.AutoIncrInsertSuffix(nil)).To(BeEmpty())
-		})
+	o.Spec("AutoIncrBindValue", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.AutoIncrBindValue()).To(matchers.Equal("null"))
 	})
 
-	Describe("CreateTableSuffix", func() {
-		Context("with an empty engine", func() {
-			BeforeEach(func() {
-				engine = ""
-				encoding = "foo"
-			})
-			It("panics", func() {
-				Expect(func() {
-					dialect.CreateTableSuffix()
-				}).To(Panic())
-			})
-		})
+	o.Spec("AutoIncrInsertSuffix", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.AutoIncrInsertSuffix(nil)).To(matchers.Equal(""))
+	})
 
-		Context("with an empty encoding", func() {
-			BeforeEach(func() {
-				engine = "foo"
-				encoding = ""
+	o.Group("CreateTableSuffix", func() {
+		o.Group("with an empty engine", func() {
+			o.BeforeEach(func(expect expect.Expectation, dialect gorp.MySQLDialect) (expect.Expectation, gorp.MySQLDialect) {
+				dialect.Engine = ""
+				return expect, dialect
 			})
-			It("panics", func() {
-				Expect(func() {
-					dialect.CreateTableSuffix()
-				}).To(Panic())
+			o.Spec("panics", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+				expect(func() { dialect.CreateTableSuffix() }).To(Panic())
 			})
 		})
 
-		Context("with an engine and an encoding", func() {
-			BeforeEach(func() {
-				engine = "foo"
-				encoding = "bar"
+		o.Group("with an empty encoding", func() {
+			o.BeforeEach(func(expect expect.Expectation, dialect gorp.MySQLDialect) (expect.Expectation, gorp.MySQLDialect) {
+				dialect.Encoding = ""
+				return expect, dialect
 			})
-			It("returns a valid suffix", func() {
-				Expect(dialect.CreateTableSuffix()).To(Equal(" engine=foo charset=bar"))
-			})
-		})
-	})
-
-	Describe("CreateIndexSuffix", func() {
-		It("returns the suffix for creating indexes", func() {
-			Expect(dialect.CreateIndexSuffix()).To(Equal("using"))
-		})
-	})
-
-	Describe("DropIndexSuffix", func() {
-		It("returns the suffix for deleting indexes", func() {
-			Expect(dialect.DropIndexSuffix()).To(Equal("on"))
-		})
-	})
-
-	Describe("TruncateClause", func() {
-		It("returns the clause for truncating a table", func() {
-			Expect(dialect.TruncateClause()).To(Equal("truncate"))
-		})
-	})
-
-	Describe("SleepClause", func() {
-		It("returns the clause for sleeping", func() {
-			Expect(dialect.SleepClause(1 * time.Second)).To(Equal("sleep(1.000000)"))
-			Expect(dialect.SleepClause(100 * time.Millisecond)).To(Equal("sleep(0.100000)"))
-		})
-	})
-
-	Describe("BindVar", func() {
-		It("returns the variable binding sequence", func() {
-			Expect(dialect.BindVar(0)).To(Equal("?"))
-		})
-	})
-
-	PDescribe("InsertAutoIncr", func() {})
-
-	Describe("QuoteField", func() {
-		It("returns the argument quoted as a field", func() {
-			Expect(dialect.QuoteField("foo")).To(Equal("`foo`"))
-		})
-	})
-
-	Describe("QuotedTableForQuery", func() {
-		var (
-			schema, table string
-
-			quotedTable string
-		)
-
-		JustBeforeEach(func() {
-			quotedTable = dialect.QuotedTableForQuery(schema, table)
-		})
-
-		Context("using the default schema", func() {
-			BeforeEach(func() {
-				schema = ""
-				table = "foo"
-			})
-			It("returns just the table", func() {
-				Expect(quotedTable).To(Equal("`foo`"))
+			o.Spec("panics", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+				expect(func() { dialect.CreateTableSuffix() }).To(Panic())
 			})
 		})
 
-		Context("with a supplied schema", func() {
-			BeforeEach(func() {
-				schema = "foo"
-				table = "bar"
-			})
-			It("returns the schema and table", func() {
-				Expect(quotedTable).To(Equal("foo.`bar`"))
-			})
+		o.Spec("with an engine and an encoding", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+			expect(dialect.CreateTableSuffix()).To(matchers.Equal(" engine=foo charset=bar"))
 		})
 	})
 
-	Describe("IfSchemaNotExists", func() {
-		It("appends 'if not exists' to the command", func() {
-			Expect(dialect.IfSchemaNotExists("foo", "bar")).To(Equal("foo if not exists"))
+	o.Spec("CreateIndexSuffix", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.CreateIndexSuffix()).To(matchers.Equal("using"))
+	})
+
+	o.Spec("DropIndexSuffix", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.DropIndexSuffix()).To(matchers.Equal("on"))
+	})
+
+	o.Spec("TruncateClause", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.TruncateClause()).To(matchers.Equal("truncate"))
+	})
+
+	o.Spec("SleepClause", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.SleepClause(1 * time.Second)).To(matchers.Equal("sleep(1.000000)"))
+		expect(dialect.SleepClause(100 * time.Millisecond)).To(matchers.Equal("sleep(0.100000)"))
+	})
+
+	o.Spec("BindVar", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.BindVar(0)).To(matchers.Equal("?"))
+	})
+
+	o.Spec("QuoteField", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.QuoteField("foo")).To(matchers.Equal("`foo`"))
+	})
+
+	o.Group("QuotedTableForQuery", func() {
+		o.Spec("using the default schema", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+			expect(dialect.QuotedTableForQuery("", "foo")).To(matchers.Equal("`foo`"))
+		})
+
+		o.Spec("with a supplied schema", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+			expect(dialect.QuotedTableForQuery("foo", "bar")).To(matchers.Equal("foo.`bar`"))
 		})
 	})
 
-	Describe("IfTableExists", func() {
-		It("appends 'if exists' to the command", func() {
-			Expect(dialect.IfTableExists("foo", "bar", "baz")).To(Equal("foo if exists"))
-		})
+	o.Spec("IfSchemaNotExists", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.IfSchemaNotExists("foo", "bar")).To(matchers.Equal("foo if not exists"))
 	})
 
-	Describe("IfTableNotExists", func() {
-		It("appends 'if not exists' to the command", func() {
-			Expect(dialect.IfTableNotExists("foo", "bar", "baz")).To(Equal("foo if not exists"))
-		})
+	o.Spec("IfTableExists", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.IfTableExists("foo", "bar", "baz")).To(matchers.Equal("foo if exists"))
 	})
-})
+
+	o.Spec("IfTableNotExists", func(expect expect.Expectation, dialect gorp.MySQLDialect) {
+		expect(dialect.IfTableNotExists("foo", "bar", "baz")).To(matchers.Equal("foo if not exists"))
+	})
+}
+
+type panicMatcher struct {
+}
+
+func Panic() panicMatcher {
+	return panicMatcher{}
+}
+
+func (m panicMatcher) Match(actual interface{}) (resultValue interface{}, err error) {
+	switch f := actual.(type) {
+	case func():
+		panicked := false
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					panicked = true
+				}
+			}()
+			f()
+		}()
+		if panicked {
+			return f, nil
+		}
+		return f, errors.New("function did not panic")
+	default:
+		return f, fmt.Errorf("%T is not func()", f)
+	}
+}
--- dialect_postgres_test.go
+++ dialect_postgres_test.go
@@ -14,184 +14,142 @@ package gorp_test
 import (
 	"database/sql"
 	"reflect"
+	"testing"
 	"time"
 
-	// ginkgo/gomega functions read better as dot-imports.
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/ginkgo/extensions/table"
-	. "github.com/onsi/gomega"
-
+	"github.com/apoydence/onpar"
+	"github.com/apoydence/onpar/expect"
+	"github.com/apoydence/onpar/matchers"
 	"github.com/go-gorp/gorp"
 )
 
-var _ = Describe("PostgresDialect", func() {
-	var (
-		lowercasefields bool
-		dialect         gorp.PostgresDialect
-	)
+func TestPostgresDialect(t *testing.T) {
+	o := onpar.New()
+	defer o.Run(t)
 
-	BeforeEach(func() {
-		lowercasefields = false
-	})
-
-	JustBeforeEach(func() {
-		dialect = gorp.PostgresDialect{
-			LowercaseFields: lowercasefields,
+	o.BeforeEach(func(t *testing.T) (expect.Expectation, gorp.PostgresDialect) {
+		return expect.New(t), gorp.PostgresDialect{
+			LowercaseFields: false,
 		}
 	})
 
-	DescribeTable("ToSqlType",
-		func(value interface{}, maxsize int, autoIncr bool, expected string) {
-			typ := reflect.TypeOf(value)
-			sqlType := dialect.ToSqlType(typ, maxsize, autoIncr)
-			Expect(sqlType).To(Equal(expected))
-		},
-		Entry("bool", true, 0, false, "boolean"),
-		Entry("int8", int8(1), 0, false, "integer"),
-		Entry("uint8", uint8(1), 0, false, "integer"),
-		Entry("int16", int16(1), 0, false, "integer"),
-		Entry("uint16", uint16(1), 0, false, "integer"),
-		Entry("int32", int32(1), 0, false, "integer"),
-		Entry("int (treated as int32)", int(1), 0, false, "integer"),
-		Entry("uint32", uint32(1), 0, false, "integer"),
-		Entry("uint (treated as uint32)", uint(1), 0, false, "integer"),
-		Entry("int64", int64(1), 0, false, "bigint"),
-		Entry("uint64", uint64(1), 0, false, "bigint"),
-		Entry("float32", float32(1), 0, false, "real"),
-		Entry("float64", float64(1), 0, false, "double precision"),
-		Entry("[]uint8", []uint8{1}, 0, false, "bytea"),
-		Entry("NullInt64", sql.NullInt64{}, 0, false, "bigint"),
-		Entry("NullFloat64", sql.NullFloat64{}, 0, false, "double precision"),
-		Entry("NullBool", sql.NullBool{}, 0, false, "boolean"),
-		Entry("Time", time.Time{}, 0, false, "timestamp with time zone"),
-		Entry("default-size string", "", 0, false, "text"),
-		Entry("sized string", "", 50, false, "varchar(50)"),
-		Entry("large string", "", 1024, false, "varchar(1024)"),
-	)
-
-	Describe("AutoIncrStr", func() {
-		It("returns the auto increment string", func() {
-			Expect(dialect.AutoIncrStr()).To(Equal(""))
-		})
-	})
-
-	Describe("AutoIncrBindValue", func() {
-		It("returns the value used to bind the auto-increment value", func() {
-			Expect(dialect.AutoIncrBindValue()).To(Equal("default"))
-		})
-	})
-
-	Describe("AutoIncrInsertSuffix", func() {
-		It("returns the suffix needed for auto-incrementing", func() {
-			cm := gorp.ColumnMap{
-				ColumnName: "foo",
-			}
-			Expect(dialect.AutoIncrInsertSuffix(&cm)).To(Equal(` returning "foo"`))
-		})
-	})
-
-	Describe("CreateTableSuffix", func() {
-		It("returns an empty suffix", func() {
-			Expect(dialect.CreateTableSuffix()).To(Equal(""))
-		})
-	})
-
-	Describe("CreateIndexSuffix", func() {
-		It("returns the suffix for creating indexes", func() {
-			Expect(dialect.CreateIndexSuffix()).To(Equal("using"))
-		})
-	})
-
-	Describe("DropIndexSuffix", func() {
-		It("returns the suffix for deleting indexes", func() {
-			Expect(dialect.DropIndexSuffix()).To(Equal(""))
-		})
-	})
-
-	Describe("TruncateClause", func() {
-		It("returns the clause for truncating a table", func() {
-			Expect(dialect.TruncateClause()).To(Equal("truncate"))
-		})
-	})
-
-	Describe("SleepClause", func() {
-		It("returns the clause for sleeping", func() {
-			Expect(dialect.SleepClause(1 * time.Second)).To(Equal("pg_sleep(1.000000)"))
-			Expect(dialect.SleepClause(100 * time.Millisecond)).To(Equal("pg_sleep(0.100000)"))
-		})
-	})
-
-	Describe("BindVar", func() {
-		It("returns the variable binding sequence", func() {
-			Expect(dialect.BindVar(0)).To(Equal("$1"))
-			Expect(dialect.BindVar(4)).To(Equal("$5"))
-		})
-	})
-
-	PDescribe("InsertAutoIncr", func() {})
-
-	Describe("QuoteField", func() {
-		It("returns the argument quoted as a field (mixed case by default)", func() {
-			Expect(dialect.QuoteField("Foo")).To(Equal(`"Foo"`))
-			Expect(dialect.QuoteField("bar")).To(Equal(`"bar"`))
-		})
-		Context("with lowercase fields", func() {
-			BeforeEach(func() {
-				lowercasefields = true
+	o.Group("ToSqlType", func() {
+		tests := []struct {
+			name     string
+			value    interface{}
+			maxSize  int
+			autoIncr bool
+			expected string
+		}{
+			{"bool", true, 0, false, "boolean"},
+			{"int8", int8(1), 0, false, "integer"},
+			{"uint8", uint8(1), 0, false, "integer"},
+			{"int16", int16(1), 0, false, "integer"},
+			{"uint16", uint16(1), 0, false, "integer"},
+			{"int32", int32(1), 0, false, "integer"},
+			{"int (treated as int32)", int(1), 0, false, "integer"},
+			{"uint32", uint32(1), 0, false, "integer"},
+			{"uint (treated as uint32)", uint(1), 0, false, "integer"},
+			{"int64", int64(1), 0, false, "bigint"},
+			{"uint64", uint64(1), 0, false, "bigint"},
+			{"float32", float32(1), 0, false, "real"},
+			{"float64", float64(1), 0, false, "double precision"},
+			{"[]uint8", []uint8{1}, 0, false, "bytea"},
+			{"NullInt64", sql.NullInt64{}, 0, false, "bigint"},
+			{"NullFloat64", sql.NullFloat64{}, 0, false, "double precision"},
+			{"NullBool", sql.NullBool{}, 0, false, "boolean"},
+			{"Time", time.Time{}, 0, false, "timestamp with time zone"},
+			{"default-size string", "", 0, false, "text"},
+			{"sized string", "", 50, false, "varchar(50)"},
+			{"large string", "", 1024, false, "varchar(1024)"},
+		}
+		for _, t := range tests {
+			o.Spec(t.name, func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+				typ := reflect.TypeOf(t.value)
+				sqlType := dialect.ToSqlType(typ, t.maxSize, t.autoIncr)
+				expect(sqlType).To(matchers.Equal(t.expected))
 			})
-			It("returns the argument quoted as a field", func() {
-				Expect(dialect.QuoteField("Foo")).To(Equal(`"foo"`))
+		}
+	})
+
+	o.Spec("AutoIncrStr", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		expect(dialect.AutoIncrStr()).To(matchers.Equal(""))
+	})
+
+	o.Spec("AutoIncrBindValue", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		expect(dialect.AutoIncrBindValue()).To(matchers.Equal("default"))
+	})
+
+	o.Spec("AutoIncrInsertSuffix", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		cm := gorp.ColumnMap{
+			ColumnName: "foo",
+		}
+		expect(dialect.AutoIncrInsertSuffix(&cm)).To(matchers.Equal(` returning "foo"`))
+	})
+
+	o.Spec("CreateTableSuffix", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		expect(dialect.CreateTableSuffix()).To(matchers.Equal(""))
+	})
+
+	o.Spec("CreateIndexSuffix", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		expect(dialect.CreateIndexSuffix()).To(matchers.Equal("using"))
+	})
+
+	o.Spec("DropIndexSuffix", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		expect(dialect.DropIndexSuffix()).To(matchers.Equal(""))
+	})
+
+	o.Spec("TruncateClause", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		expect(dialect.TruncateClause()).To(matchers.Equal("truncate"))
+	})
+
+	o.Spec("SleepClause", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		expect(dialect.SleepClause(1 * time.Second)).To(matchers.Equal("pg_sleep(1.000000)"))
+		expect(dialect.SleepClause(100 * time.Millisecond)).To(matchers.Equal("pg_sleep(0.100000)"))
+	})
+
+	o.Spec("BindVar", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		expect(dialect.BindVar(0)).To(matchers.Equal("$1"))
+		expect(dialect.BindVar(4)).To(matchers.Equal("$5"))
+	})
+
+	o.Group("QuoteField", func() {
+		o.Spec("By default, case is preserved", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+			expect(dialect.QuoteField("Foo")).To(matchers.Equal(`"Foo"`))
+			expect(dialect.QuoteField("bar")).To(matchers.Equal(`"bar"`))
+		})
+
+		o.Group("With LowercaseFields set to true", func() {
+			o.BeforeEach(func(expect expect.Expectation, dialect gorp.PostgresDialect) (expect.Expectation, gorp.PostgresDialect) {
+				dialect.LowercaseFields = true
+				return expect, dialect
+			})
+
+			o.Spec("fields are lowercased", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+				expect(dialect.QuoteField("Foo")).To(matchers.Equal(`"foo"`))
 			})
 		})
 	})
 
-	Describe("QuotedTableForQuery", func() {
-		var (
-			schema, table string
-
-			quotedTable string
-		)
-
-		JustBeforeEach(func() {
-			quotedTable = dialect.QuotedTableForQuery(schema, table)
+	o.Group("QuotedTableForQuery", func() {
+		o.Spec("using the default schema", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+			expect(dialect.QuotedTableForQuery("", "foo")).To(matchers.Equal(`"foo"`))
 		})
 
-		Context("using the default schema", func() {
-			BeforeEach(func() {
-				schema = ""
-				table = "foo"
-			})
-			It("returns just the table", func() {
-				Expect(quotedTable).To(Equal(`"foo"`))
-			})
-		})
-
-		Context("with a supplied schema", func() {
-			BeforeEach(func() {
-				schema = "foo"
-				table = "bar"
-			})
-			It("returns the schema and table", func() {
-				Expect(quotedTable).To(Equal(`foo."bar"`))
-			})
+		o.Spec("with a supplied schema", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+			expect(dialect.QuotedTableForQuery("foo", "bar")).To(matchers.Equal(`foo."bar"`))
 		})
 	})
 
-	Describe("IfSchemaNotExists", func() {
-		It("appends 'if not exists' to the command", func() {
-			Expect(dialect.IfSchemaNotExists("foo", "bar")).To(Equal("foo if not exists"))
-		})
+	o.Spec("IfSchemaNotExists", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		expect(dialect.IfSchemaNotExists("foo", "bar")).To(matchers.Equal("foo if not exists"))
 	})
 
-	Describe("IfTableExists", func() {
-		It("appends 'if exists' to the command", func() {
-			Expect(dialect.IfTableExists("foo", "bar", "baz")).To(Equal("foo if exists"))
-		})
+	o.Spec("IfTableExists", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		expect(dialect.IfTableExists("foo", "bar", "baz")).To(matchers.Equal("foo if exists"))
 	})
 
-	Describe("IfTableNotExists", func() {
-		It("appends 'if not exists' to the command", func() {
-			Expect(dialect.IfTableNotExists("foo", "bar", "baz")).To(Equal("foo if not exists"))
-		})
+	o.Spec("IfTableNotExists", func(expect expect.Expectation, dialect gorp.PostgresDialect) {
+		expect(dialect.IfTableNotExists("foo", "bar", "baz")).To(matchers.Equal("foo if not exists"))
 	})
-})
+}
