5b5e9b564e5207be5dc3f9d1a921150ca8e7bfc4
nawa <navatski@gmail.com>
2018-09-24 14:56:07 +0300

    Fix few typos in hooks comments

--- hooks.go
+++ hooks.go
@@ -13,37 +13,37 @@ package gorp
 
 //++ TODO v2-phase3: HasPostGet => PostGetter, HasPostDelete => PostDeleter, etc.
 
-// PostUpdate() will be executed after the GET statement.
+// HasPostGet provides PostGet() which will be executed after the GET statement.
 type HasPostGet interface {
 	PostGet(SqlExecutor) error
 }
 
-// PostUpdate() will be executed after the DELETE statement
+// HasPostDelete provides PostDelete() which will be executed after the DELETE statement
 type HasPostDelete interface {
 	PostDelete(SqlExecutor) error
 }
 
-// PostUpdate() will be executed after the UPDATE statement
+// HasPostUpdate provides PostUpdate() which will be executed after the UPDATE statement
 type HasPostUpdate interface {
 	PostUpdate(SqlExecutor) error
 }
 
-// PostInsert() will be executed after the INSERT statement
+// HasPostInsert provides PostInsert() which will be executed after the INSERT statement
 type HasPostInsert interface {
 	PostInsert(SqlExecutor) error
 }
 
-// PreDelete() will be executed before the DELETE statement.
+// HasPreDelete provides PreDelete() which will be executed before the DELETE statement.
 type HasPreDelete interface {
 	PreDelete(SqlExecutor) error
 }
 
-// PreUpdate() will be executed before UPDATE statement.
+// HasPreUpdate provides PreUpdate() which will be executed before UPDATE statement.
 type HasPreUpdate interface {
 	PreUpdate(SqlExecutor) error
 }
 
-// PreInsert() will be executed before INSERT statement.
+// HasPreInsert provides PreInsert() which will be executed before INSERT statement.
 type HasPreInsert interface {
 	PreInsert(SqlExecutor) error
 }
