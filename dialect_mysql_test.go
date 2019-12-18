5e7a584a90fa5a58f18491b05f71274162e93c73
Songmu <y.songmu@gmail.com>
2019-12-18 15:04:36 +0900

    fix import path in testing

--- dialect_mysql_test.go
+++ dialect_mysql_test.go
@@ -14,10 +14,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/go-gorp/gorp/v3"
 	"github.com/poy/onpar"
 	"github.com/poy/onpar/expect"
 	"github.com/poy/onpar/matchers"
-	"github.com/go-gorp/gorp"
 )
 
 func TestMySQLDialect(t *testing.T) {
--- gorp_test.go
+++ gorp_test.go
@@ -23,7 +23,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/go-gorp/gorp"
+	"github.com/go-gorp/gorp/v3"
 
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
