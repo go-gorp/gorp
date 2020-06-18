c1d3cf571022d924a72f0e252f35a660335cbc97
Mario de Frutos <mario@defrutos.org>
2020-06-18 17:52:17 +0200

    wip

--- test_all.sh
+++ test_all.sh
@@ -3,23 +3,23 @@
 # on macs, you may need to:
 # export GOBUILDFLAG=-ldflags -linkmode=external
 
-coveralls_testflags="-v -covermode=count -coverprofile=coverage.out"
+coveralls_testflags="-covermode=count -coverprofile=coverage.out"
 
 echo "Running unit tests"
 ginkgo -r -race -randomizeAllSpecs -keepGoing -- -test.run TestGorp
 
-echo "Testing against mysql"
-export GORP_TEST_DSN=gorptest/gorptest/gorptest
+echo "Testing against mymysql"
+export GORP_TEST_DSN="tcp:192.168.254.7:3306*gorptest/gorptest/gorptest"
 export GORP_TEST_DIALECT=mysql
-go test $coveralls_testflags $GOBUILDFLAG $@ .
+go test $coveralls_testflags $GOBUILDFLAG -test.run=$@ .
 
 echo "Testing against gomysql"
-export GORP_TEST_DSN=gorptest:gorptest@/gorptest
+export GORP_TEST_DSN=gorptest:gorptest@192.168.254.7/gorptest
 export GORP_TEST_DIALECT=gomysql
 go test $coveralls_testflags $GOBUILDFLAG $@ .
 
 echo "Testing against postgres"
-export GORP_TEST_DSN="user=gorptest password=gorptest dbname=gorptest sslmode=disable"
+export GORP_TEST_DSN="host=192.168.254.5 user=gorptest password=gorptest dbname=gorptest sslmode=disable"
 export GORP_TEST_DIALECT=postgres
 go test $coveralls_testflags $GOBUILDFLAG $@ .
 
@@ -30,7 +30,7 @@ go test $coveralls_testflags $GOBUILDFLAG $@ .
 rm -f /tmp/gorptest.bin
 
 case $(go version) in
-  *go1.4*)
+  *go1.13*)
     if [ "$(type -p goveralls)" != "" ]; then
 	  goveralls -covermode=count -coverprofile=coverage.out -service=travis-ci
     elif [ -x $HOME/gopath/bin/goveralls ]; then
@@ -38,4 +38,4 @@ case $(go version) in
     fi
   ;;
   *) ;;
-esac
+esac%
